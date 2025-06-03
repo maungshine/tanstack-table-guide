544e22e094933c932d80a65d0ac61c646387ff53
Maung Shine <Maung.Shine@fusionsol.com>
2025-06-03 23:34:54 +0630

    initial commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+// docs/.vitepress/config.ts
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'TanStack Table Guide',
+  description: 'A clear, simple guide to TanStack Table v8+',
+  base: '/tanstack-table-guide/',
+})
